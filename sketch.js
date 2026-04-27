1d2b61916cf0cbccb069b0c437bc29c891a42101
kimgyurin <koong5111@naver.com>
2026-04-27 21:59:07 +0900

    39th

--- sketch.js
+++ sketch.js
@@ -15,9 +15,9 @@ let height = 1536;
 let beansGenerated = false;
 
 let energy = 3;
-let enemies = [];   
+let enemies = [];
 let gameState = "playing"; // "playing", "win", "lose"
-let invincible = false; 
+let invincible = false;
 let invincibleTimer = 0;
 
 function preload() {
@@ -555,19 +555,19 @@ function eatBeans() {
 
 function generateEnemies() {
     enemies = [];
-    // 길 위의 랜덤 위치에 적 5개 생성
     let pathCells = [];
     for (let x = 100; x < width - 100; x += beanSpacing) {
         for (let y = 100; y < height - 100; y += beanSpacing) {
-            if (isPath(x, y)) pathCells.push({x, y});
+            if (isPath(x, y)) pathCells.push({ x, y });
         }
     }
     for (let i = 0; i < 5; i++) {
         let pos = random(pathCells);
         enemies.push({
-            x: pos.x, y: pos.y,
-            dx: random([-2, 2]),  // 이동 방향
-            dy: 0
+            x: pos.x,
+            y: pos.y,
+            dx: random([-2, 2]),
+            dy: 0,
         });
     }
 }
